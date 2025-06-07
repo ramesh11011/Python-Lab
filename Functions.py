441f6e5e2f4eec5feebc9f6661d5b7f11cada7d0
Ramesh11011 <programming.ramesh@gmail.com>
2025-06-07 10:37:48 +0530

    Created Functions.py

---
+++ Functions.py
@@ -0,0 +1,8 @@
+# Basic Functions in Python 
+
+def twoSum(num1, num2):
+    return num1+num2
+
+a = int(input("Enter the value of a : "))
+b = int(input("Enter the value of a : "))
+print(twoSum(a,b))
