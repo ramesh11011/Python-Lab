f2cd06b8da8af360128c198ed1f4a8dc57a69012
Ramesh11011 <programming.ramesh@gmail.com>
2025-06-07 10:51:19 +0530

    Dynamic function for addition of numbers

--- Functions.py
+++ Functions.py
@@ -6,3 +6,15 @@ def twoSum(num1, num2):
 a = int(input("Enter the value of a : "))
 b = int(input("Enter the value of a : "))
 print(twoSum(a,b))
+
+#dynamic function for adding numbers 
+#creating Dynamic Function
+def twoSum2(*number):
+    # print(number)
+    ans = 0;
+    for itr in number:
+        # print(itr, type(itr))
+        ans = ans + itr;
+    print(ans)
+
+twoSum2(12, 13, 8, 12,45)
