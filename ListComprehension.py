f88a23251ebae951d0d434381b1a00bd8b223b79
Ramesh11011 <programming.ramesh@gmail.com>
2025-06-08 09:37:57 +0530

    List Comprehension Completed

---
+++ ListComprehension.py
@@ -0,0 +1,9 @@
+#List comprehension
+
+# Taking list as an input
+mylist = [ele for ele in range(5)]
+
+#type casting of the element of the list , taking the elements of the list as input
+mylist1 = [int(ele) for ele in input().split()]
+print(mylist)
+print(mylist1)
